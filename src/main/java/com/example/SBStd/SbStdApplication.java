bd6228878333a5e3e936a96fc44701bb0b6ab0cc
CiroY <ycr5007@gmail.com>
2024-08-29 16:17:05 +0900

    first commit

---
+++ src/main/java/com/example/SBStd/SbStdApplication.java
@@ -0,0 +1,13 @@
+package com.example.SBStd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbStdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbStdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SBStd/SbStdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SBStd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbStdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
